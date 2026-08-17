0ba09c7fa9b74c38cc1caebbbe7683514890a602
fazlerabbi13 <mohammad0132794@gmail.com>
2026-08-17 23:22:59 +0600

    completed banner section with three banner image.

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -2,8 +2,9 @@ import logo from "../../assets/logo.png";
 
 const Logo = () => {
     return (
-        <div>
+        <div className="flex justify-center items-center">
             <img src={logo} alt="" />
+            <h3 className="text-2xl font-semibold -ms-3">zapShift</h3>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../Banner/Banner";
+
 const Home = () => {
     return (
         <div>
-            <h3>this is home</h3>
+            <Banner></Banner>
         </div>
     );
 };
