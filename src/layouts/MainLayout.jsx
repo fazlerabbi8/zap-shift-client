79f861c4f849bdc36b1acfd9649a9981b565ed2e
fazlerabbi13 <mohammad0132794@gmail.com>
2026-08-17 22:00:42 +0600

    setting up navbar and footer with logo.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Navbar from "../pages/shared/Navbar/Navbar";
+import Footer from "../pages/shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div className="container mx-auto p-6">
-      <Outlet />
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
