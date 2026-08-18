ca5e58e4d4effeee7645a3075f57bac4d4406152
fazlerabbi13 <mohammad0132794@gmail.com>
2026-08-18 19:53:09 +0600

    completed search for coordinate using flyto.

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,35 +1,83 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useLoaderData } from "react-router";
+import { useRef } from "react";
 
 const Coverage = () => {
   const position = [23.685, 90.3563];
   const serviceCenter = useLoaderData();
-  console.log(serviceCenter)
+  
+  const mapRef = useRef(null);
+
+  const handleSearch = e =>{
+    e.preventDefault();
+
+    const location = e.target.location.value;
+    const district = serviceCenter.find(a => a.district.toLowerCase().includes(location.toLowerCase()));
+
+    if(district){
+      const coord = [district.latitude, district.longitude];
+      mapRef.current.flyTo(coord, 15);
+    }
+
+  }
+
   return (
     <div>
-      <div></div>
-      <div></div>
+      <div>
+        <h3 className="text-3xl font-semibold mb-5">Our Services 64 Areas</h3>
+      </div>
+      {/* search area */}
+      <div className="mb-5">
+        <form onSubmit={handleSearch}>
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" className="grow" 
+            name="location"
+            placeholder="Search" />
+            <kbd className="kbd kbd-sm">⌘</kbd>
+            <kbd className="kbd kbd-sm">K</kbd>
+          </label>
+        </form>
+      </div>
       <div className="border border-gray-400">
         <MapContainer
           center={position}
           zoom={8}
+          ref={mapRef}
           style={{ height: "500px", width: "100%" }}
         >
           <TileLayer
             attribution='&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {
-            serviceCenter.map((center, idx) => <Marker key={center.idx}
-            position={[center.latitude, center.longitude]}>
-            <Popup>
-              <strong>{center.district}</strong>
-              <br />
-              Service Area: {center.covered_area.join(', ')}
-            </Popup>
-          </Marker>)
-          }
+          {serviceCenter.map((center, idx) => (
+            <Marker
+              key={idx}
+              position={[center.latitude, center.longitude]}
+            >
+              <Popup>
+                <strong>{center.district}</strong>
+                <br />
+                Service Area: {center.covered_area.join(", ")}
+              </Popup>
+            </Marker>
+          ))}
         </MapContainer>
       </div>
     </div>
