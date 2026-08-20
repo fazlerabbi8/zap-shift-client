d5f47bf4bf020807eebafc43bfa38c1e21c00c3a
fazlerabbi13 <mohammad0132794@gmail.com>
2026-08-20 19:00:47 +0600

    implemented private route.

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -1,18 +1,18 @@
 
-// import useAuth from "../Hooks/useAuth";
-// import { Navigate } from "react-router";
+import useAuth from "../Hooks/useAuth";
+import { Navigate } from "react-router";
 
-// const PrivateRoute = ({ children }) => {
-//   const { user, loading } = useAuth();
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
 
-//   if (loading) {
-//     return <span className="loading loading-spinner text-neutral"></span>;
-//   }
+  if (loading) {
+    return <span className="loading loading-spinner text-neutral"></span>;
+  }
 
-//   if (!user) {
-//     return <Navigate to={"/login"}></Navigate>;
-//   }
-//   return children;
-// };
+  if (!user) {
+    return <Navigate to={"/login"}></Navigate>;
+  }
+  return children;
+};
 
-// export default PrivateRoute;
+export default PrivateRoute;
