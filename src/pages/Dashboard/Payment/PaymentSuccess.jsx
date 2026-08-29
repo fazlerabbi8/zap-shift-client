b7390bded74b0d11128f626ab396e62a6211c0e4
fazlerabbi13 <mohammad0132794@gmail.com>
2026-08-29 12:27:44 +0600

    showing tracking id and transaction id in ui.

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [paymentInfo, setPaymentInfo] = useState({});
   const axiosSecure = useAxiosSecure();
   const sessionId = searchParams.get("session_id");
 
   useEffect(() => {
-    if(sessionId){
-        axiosSecure.patch(`/payment-sucess?session_id=${sessionId}`)
-        .then(res => {
-            console.log(res.data)
-        })
+    if (sessionId) {
+      axiosSecure
+        .patch(`/payment-sucess?session_id=${sessionId}`)
+        .then((res) => {
+          console.log(res.data);
+          setPaymentInfo({
+            transactionId: res.data.transactionId,
+            trackingId: res.data.trackingId,
+          });
+        });
     }
   }, [sessionId, axiosSecure]);
   return (
@@ -20,6 +26,10 @@ const PaymentSuccess = () => {
       <h3 className="text-green-600 text-2xl font-semibold text-center mt-20">
         Your Payment Successfull
       </h3>
+      <div className="mt-5 text-center">
+        <p><span className="text-orange-400">Transaxtion Id:</span> {paymentInfo.transactionId}</p>
+        <p><span className="text-orange-400">Tracking Id:</span> {paymentInfo.trackingId}</p>
+      </div>
     </div>
   );
 };
