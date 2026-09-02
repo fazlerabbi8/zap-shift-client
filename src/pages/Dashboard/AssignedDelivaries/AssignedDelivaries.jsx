27d3e1d60dc9f6d70d82cc81c2b4055277f9451e
fazlerabbi13 <mohammad0132794@gmail.com>
2026-09-02 14:06:18 +0600

    implemented delivary accepted functionality.rider can accept delivary.

--- src/pages/Dashboard/AssignedDelivaries/AssignedDelivaries.jsx
+++ src/pages/Dashboard/AssignedDelivaries/AssignedDelivaries.jsx
@@ -1,19 +1,37 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import useAuth from "../../../Hooks/useAuth";
+import Swal from "sweetalert2";
+import { Space } from "lucide-react";
 const AssignedDelivaries = () => {
   const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["parcels", user?.email, "driver-assigned"],
     queryFn: async () => {
-      const res = await axiosSecure.get(`/parcels/rider?riderEmail=${user.email}&penddingStatus=driver-assigned`);
+      const res = await axiosSecure.get(
+        `/parcels/rider?riderEmail=${user.email}&penddingStatus=driver-assigned`,
+      );
       return res.data;
     },
   });
+
+  const handleAcceptDelivary = (parcel) => {
+    const statusInfo = { penddingStatus: "rider-arriving" };
+    axiosSecure
+      .patch(`/parcels/${parcel._id}/status`, statusInfo)
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          refetch();
+          Swal.fire("Thank you for accepting");
+        }
+      });
+  };
   return (
     <div className="p-5">
-      <h3 className="text-4xl font-semibold">All Assigned Delivaries:{parcels.length}</h3>
+      <h3 className="text-4xl font-semibold">
+        All Assigned Delivaries:{parcels.length}
+      </h3>
 
       <div className="overflow-x-auto">
         <table className="table table-zebra">
@@ -23,7 +41,7 @@ const AssignedDelivaries = () => {
               <th></th>
               <th>Name</th>
               <th>Actions</th>
-              <th>Favorite Color</th>
+              <th>parcelType</th>
             </tr>
           </thead>
           <tbody>
@@ -33,10 +51,24 @@ const AssignedDelivaries = () => {
                 <th>{idx + 1}</th>
                 <td>{parcel.parcelName}</td>
                 <td className="space-x-1">
-                    <button className="btn btn-primary text-black">Accept</button>
-                    <button className="btn btn-error text-black">Reject</button>
+                  {parcel.penddingStatus === "driver-assigned" ? (
+                    <>
+                      <button
+                        onClick={() => handleAcceptDelivary(parcel)}
+                        className="btn btn-primary text-black"
+                      >
+                        Accept
+                      </button>
+
+                      <button className="btn btn-error text-black">
+                        Reject
+                      </button>
+                    </>
+                  ) : (
+                    <span className="badge badge-success">Delivary Accepted</span>
+                  )}
                 </td>
-                <td>Blue</td>
+                <td>{parcel.parcelType}</td>
               </tr>
             ))}
           </tbody>
